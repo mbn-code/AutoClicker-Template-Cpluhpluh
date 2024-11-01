14c3a82fe38203fbe89badb1ab41401554b8f8a5
mbn-code <66748817+mbn-code@users.noreply.github.com>
2024-11-01 15:19:16 +0100

    Refactor clickLoop method in AutoClicker: move mouse click event creation inside loop and ensure proper resource release

--- includes/autoclicker.cpp
+++ includes/autoclicker.cpp
@@ -23,23 +23,25 @@ bool AutoClicker::isRunning() const {
 }
 
 void AutoClicker::clickLoop() {
-    CGEventRef click_down = CGEventCreateMouseEvent(
-        NULL, kCGEventLeftMouseDown,
-        CGEventGetLocation(CGEventCreate(NULL)),
-        kCGMouseButtonLeft
-    );
-    CGEventRef click_up = CGEventCreateMouseEvent(
-        NULL, kCGEventLeftMouseUp,
-        CGEventGetLocation(CGEventCreate(NULL)),
-        kCGMouseButtonLeft
-    );
-
     while (running) {
+        // Simulate mouse click
+        CGEventRef click_down = CGEventCreateMouseEvent(
+            NULL, kCGEventLeftMouseDown,
+            CGEventGetLocation(CGEventCreate(NULL)),
+            kCGMouseButtonLeft
+        );
+        CGEventRef click_up = CGEventCreateMouseEvent(
+            NULL, kCGEventLeftMouseUp,
+            CGEventGetLocation(CGEventCreate(NULL)),
+            kCGMouseButtonLeft
+        );
+
         CGEventPost(kCGHIDEventTap, click_down);
         CGEventPost(kCGHIDEventTap, click_up);
+
+        CFRelease(click_down);
+        CFRelease(click_up);
+
         std::this_thread::sleep_for(std::chrono::milliseconds(interval_ms));
     }
-
-    CFRelease(click_down);
-    CFRelease(click_up);
 }
